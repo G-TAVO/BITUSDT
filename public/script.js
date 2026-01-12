ab53ff14568785b331cce63ad0b28bc9f0dd88a2
Gustavo Enrique Vega Mercado <enriquevega201618@gmail.com>
2026-01-12 16:47:50 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,39 +1,56 @@
+let logueado=false;
+let saldo=0,hoy=0,total=0;
+let usuarios=0, invertido=0;
+
 function show(id){
 document.querySelectorAll('.card')
 .forEach(x=>x.style.display="none");
 document.getElementById(id).style.display="block";
 }
 
-show("register");
-
-// SIMULACIÓN
-let saldo=0, hoy=0, total=0, usuarios=0, invertido=0;
+show("login");
 
 function register(){
 usuarios++;
-alert("Registro exitoso (simulado)");
+alert("Registro exitoso");
 document.getElementById("u").innerText=usuarios;
+show("login");
 }
 
 function login(){
-alert("Login correcto (simulado)");
+logueado=true;
+alert("Sesión iniciada");
 show("panel");
 }
 
 function invertir(){
-let m=Number(monto.value);
-let ganancia=m*0.15;
+
+if(!logueado){
+alert("Debes iniciar sesión");
+return;
+}
+
+let m=document.getElementById("monto").value;
+
+if(m==""){
+alert("Seleccione monto");
+return;
+}
+
+m=Number(m);
+
+let ganancia=m*0.04; // 4%
+
 saldo+=ganancia;
 hoy+=ganancia;
 total+=ganancia;
 invertido+=m;
 
-saldoUI();
+document.getElementById("saldo").innerText=saldo.toFixed(2);
+document.getElementById("hoy").innerText=hoy.toFixed(2);
+document.getElementById("total").innerText=total.toFixed(2);
 document.getElementById("inv").innerText=invertido;
+
+alert("Invertido "+m+" USDT | Ganancia "+ganancia.toFixed(2));
 }
 
-function saldoUI(){
-saldo.innerText=saldo.toFixed(2);
-hoy.innerText=hoy.toFixed(2);
-total.innerText=total.toFixed(2);
-}
