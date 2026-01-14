3877654b2af3b1f9d0b286b82b4b879f39dfdba6
Gustavo Enrique Vega Mercado <enriquevega201618@gmail.com>
2026-01-14 17:06:24 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,65 +1,25 @@
-// VERIFICAR SESIÓN
-if(localStorage.getItem("rol")=="admin"){
-if(location.pathname.includes("login") ||
-location.pathname.includes("index")){
-location="admin.html";
-}
-}
 
-if(localStorage.getItem("rol")=="user"){
-if(location.pathname.includes("admin")){
-location="panel.html";
-}
+/* ======================
+   MOSTRAR SECCIONES
+====================== */
+function mostrar(id){
+document.querySelectorAll(".card")
+.forEach(d=>d.classList.add("oculto"));
+
+document.getElementById(id)
+.classList.remove("oculto");
 }
 
-// REGISTRO
-async function registrar(){
-
-const data={
-email:rCorreo.value,
-password:rPass.value
-};
-
-const res = await fetch("/api/register",{
-method:"POST",
-headers:{"Content-Type":"application/json"},
-body:JSON.stringify(data)
-});
-
-let j = await res.json();
-alert(j.msg);
+/* ======================
+   GUARDAR WALLET
+====================== */
+function guardarWallet(){
+alert("Wallet guardada (demo)");
 }
 
-// LOGIN
-async function login(){
-
-const data={
-email:loginEmail.value,
-password:loginPass.value
-};
-
-const res = await fetch("/api/login",{
-method:"POST",
-headers:{"Content-Type":"application/json"},
-body:JSON.stringify(data)
-});
-
-let j = await res.json();
-if(!j.ok) return alert(j.msg);
-
-// GUARDAR ROL
-localStorage.setItem("rol",j.rol);
-
-if(j.rol=="admin"){
-location="admin.html";
-}else{
-location="panel.html";
+/* ======================
+   SOLICITAR INVERSIÓN
+====================== */
+function solicitar(){
+alert("Solicitud enviada al admin");
 }
-}
-
-// CERRAR
-function logout(){
-localStorage.clear();
-location="login.html";
-}
-
