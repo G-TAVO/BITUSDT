cbd6cdd30cbbc2a25afb398ce1e4403d1a7b0f69
Gustavo Enrique Vega Mercado <enriquevega201618@gmail.com>
2026-01-13 20:49:38 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,27 +1,3 @@
-function show(id){
-document.querySelectorAll("section").forEach(s=>{
-s.classList.remove("active");
-});
-document.getElementById(id).classList.add("active");
-}
-
-// REGISTRO
-async function register(){
-const data={
-nombre:nombre.value,
-email:email.value,
-password:password.value
-};
-
-const res = await fetch("/api/register",{
-method:"POST",
-headers:{"Content-Type":"application/json"},
-body:JSON.stringify(data)
-});
-
-const json = await res.json();
-msg.innerText=json.msg;
-}
 
 // LOGIN
 async function login(){
@@ -42,25 +18,19 @@ loginMsg.innerText=json.msg;
 
 if(json.ok){
 
-// ADMIN
-if(
-loginEmail.value=="admin@bitusdt.com" &&
-loginPass.value=="amAdmin1998"
-){
-    localStorage.setItem("rol","admin");
-    window.location="Admin.html";
+if(json.rol=="admin"){
+ window.location="/admin";
 }else{
-    localStorage.setItem("rol","user");
-    window.location="Panel.html";
+ window.location="panel.html";
 }
 
 }
 }
 
 // CERRAR SESIÓN
-function logout(){
-localStorage.clear();
-window.location="login.html";
+async function logout(){
+ await fetch("/api/logout");
+ window.location="login.html";
 }
 
 
