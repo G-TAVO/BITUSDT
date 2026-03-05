0405c412849a9a85ac783cfe64d5b513c4304dff
Gustavo Enrique Vega Mercado <enriquevega201618@gmail.com>
2026-03-04 20:11:29 -0500

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,42 @@
+const CACHE_NAME = "bitusdt-v2";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192x192.png",
+  "/icon-512x512.png"
+];
+
 self.addEventListener("install", (event) => {
-  console.log("Service Worker instalado");
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("Service Worker activado");
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
 });
